b9eef486fa57b0a5aca98c1b1715f368e9c3b422
Aman Ahmed <aman16.aa17@gmail.com>
2022-11-14 09:46:58 +0100

    Sorting and Filtering with Linq

--- LINQToObjectsAndQueryOperators/Program.cs
+++ LINQToObjectsAndQueryOperators/Program.cs
@@ -14,8 +14,38 @@ namespace LINQToObjectsAndQueryOperators
             UniversityManager um = new UniversityManager();
 
             um.MaleStudents();
-
             um.FemaleStudents();
+            um.SortStudentsByAge();
+            um.AllStudentsFromBeijingTech();
+
+            int[] someInts = { 30, 12, 4, 3, 12 };
+            IEnumerable<int> sortedInts = from i in someInts orderby i select i;
+            IEnumerable<int> reversedInts = sortedInts.Reverse();
+
+            foreach (int i in reversedInts)
+            {
+                Console.WriteLine(i);
+            }
+
+            IEnumerable<int> reversedSortedInts = from i in someInts orderby i descending select i;
+
+            foreach (int i in reversedSortedInts)
+            {
+                Console.WriteLine(i);
+            }
+
+            // Console.WriteLine("Enter University Id: ");
+            // string input = Console.ReadLine();
+            // try
+            // {
+            //     int inputAsInt = Convert.ToInt32(input);
+
+            //     um.AllStudentsFromThatUni(inputAsInt);
+            // }
+            // catch (Exception)
+            // {
+            //     Console.WriteLine("Wrong value");
+            // }
 
             Console.ReadKey();
         }
@@ -50,7 +80,7 @@ namespace LINQToObjectsAndQueryOperators
             IEnumerable<Student> maleStudents = from student in students where student.Gender == "male" select student;
             Console.WriteLine("Male - Students: ");
 
-            foreach(Student student in maleStudents)
+            foreach (Student student in maleStudents)
             {
                 student.Print();
             }
@@ -62,6 +92,50 @@ namespace LINQToObjectsAndQueryOperators
             Console.WriteLine("Female - Students: ");
 
             foreach (Student student in femaleStudents)
+            {
+                student.Print();
+            }
+        }
+
+        public void SortStudentsByAge()
+        {
+            var sortedStudents = from student in students orderby student.Age select student;
+
+            Console.WriteLine("Students sorted by Age: ");
+
+            foreach (Student student in sortedStudents)
+            {
+                student.Print();
+            }
+        }
+
+        public void AllStudentsFromBeijingTech()
+        {
+            IEnumerable<Student> bjtStudents =
+                from student in students
+                join university in universities on student.UniversityId equals university.Id
+                where university.Name == "Beijing Tech"
+                select student;
+
+            Console.WriteLine("Students from Beijing Tech: ");
+
+            foreach (Student student in bjtStudents)
+            {
+                student.Print();
+            }
+        }
+
+        public void AllStudentsFromThatUni(int Id)
+        {
+            IEnumerable<Student> myStudents =
+                from student in students
+                join university in universities on student.UniversityId equals university.Id
+                where university.Id == Id
+                select student;
+
+            Console.WriteLine("Students from that uni: {0}.", Id);
+
+            foreach (Student student in myStudents)
             {
                 student.Print();
             }
